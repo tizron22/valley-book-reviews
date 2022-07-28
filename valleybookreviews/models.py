6e9a9d5caf0494e8b3920d35a3066bd0ab94ad42
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-07-28 20:05:48 +0100

    feat: rm unneeded columns from db

--- valleybookreviews/models.py
+++ valleybookreviews/models.py
@@ -1,16 +1,14 @@
-from valleybookreviews import db
+from valleybookreviews import db, UserMixin, datetime
 
 
-class User(db.Model):
+class Users(db.Model, UserMixin):
     # schema for User Details
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(25), unique=True, nullable=False)
     first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(320), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    created_date = db.Column(db.Date, nullable=False)
-    modified_date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
         return self.user_name
