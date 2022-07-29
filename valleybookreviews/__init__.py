3caab74884c60f8e423d2596bd09e1defc89879b
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-07-29 05:17:39 +0100

    feat: add classes for flask login valid into init

--- valleybookreviews/__init__.py
+++ valleybookreviews/__init__.py
@@ -2,7 +2,7 @@ import os
 import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 
