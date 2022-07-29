4de8e30b08b36b150cbb8758d90dc525e5d47501
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-07-29 06:24:09 +0100

    feat: add script to show/hide my account menu

--- valleybookreviews/static/js/script.js
+++ valleybookreviews/static/js/script.js
@@ -17,6 +17,24 @@ const showSideBar = (sideBarElement) => {
 };
 
 /**
+ * This will hide the account menu bar.
+ * @param {string} accountMenuElement
+ */
+const hideAccountMenu = (accountMenuElement) => {
+  accountMenuElement.classList.remove("d-block");
+  accountMenuElement.classList.add("d-none");
+};
+
+/**
+ * This will show the account menu bar.
+ * @param {string} accountMenuElement
+ */
+const showAccountMenu = (accountMenuElement) => {
+  accountMenuElement.classList.remove("d-none");
+  accountMenuElement.classList.add("d-block");
+};
+
+/**
  * If the sidebar is showing this will hide it and visa-versa if the sidebar is hiding, it will show it.
  * @param {string} sideBar
  */
@@ -30,6 +48,19 @@ const hamburgerMenu = (sideBar) => {
 };
 
 /**
+ * If the My Account menu is showing this will hide it and visa-versa if the menu is hiding, it will show it.
+ * @param {string} accountMenu
+ */
+const myAccountMenu = (accountMenu) => {
+  const accountMenuOn = accountMenu.classList.contains("d-block");
+  if (accountMenuOn) {
+    hideAccountMenu(accountMenu);
+  } else {
+    showAccountMenu(accountMenu);
+  }
+};
+
+/**
  * This will add an event listener to the hamburger icon when clicked.
  */
 const hamburgerSelector = document.querySelector("#hamburgerMenuSelector");
@@ -39,10 +70,19 @@ hamburgerSelector.addEventListener("click", () => {
 });
 
 /**
- * This will close the sidebar when the cross at the top is clicked.
+ * This will hide the sidebar when the cross at the top is clicked.
  */
 const sideBarCross = document.querySelector("#openSidebarCross");
 sideBarCross.addEventListener("click", () => {
   const sideBarMenu = document.querySelector("#sideBar");
   hideSideBar(sideBarMenu);
+});
+
+/**
+ * This will hide or show the My Account menu bar when the account icon is clicked.
+ */
+const accountMenuIcon = document.querySelector("#myAccountSelector");
+accountMenuIcon.addEventListener("click", () => {
+  const accountSelector = document.querySelector("#accountMenu");
+  myAccountMenu(accountSelector);
 });
