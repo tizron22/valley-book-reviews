138ce3e3b4e1a9fcf9ca8a6a7c91511cbf94eabb
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-08-01 05:50:26 +0100

    feat: add functionality to get data for review & add to db

--- valleybookreviews/reviews/views.py
+++ valleybookreviews/reviews/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, redirect, url_for
 from flask_login import current_user, login_required
+from valleybookreviews.reviews.classes import UserReviews
 
 user_reviews = Blueprint('user_reviews', __name__)
 
@@ -7,5 +8,17 @@ user_reviews = Blueprint('user_reviews', __name__)
 @user_reviews.route("/myreviews", methods=["GET", "POST"])
 @login_required
 def myreviews():
+
+    if request.method == "POST":
+
+        new_review = UserReviews(
+            user_id=current_user.user_name,
+            book_name=request.form.get("bookName"),
+            author_name=request.form.get("authorName"),
+            image_link=request.form.get("imageLink"),
+            review_text=request.form.get("reviewText")
+        )
+        new_review.add_user_review()
+        # return redirect("myreview")
 
     return render_template("myreviews.html")
