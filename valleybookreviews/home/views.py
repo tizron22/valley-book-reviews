ab3c19816526c2bd55e0b585fc74f731038e61b1
Robert Lewis <robertlewis@Roberts-MacBook-Pro.local>
2022-08-02 19:25:15 +0100

    feat: add query for random review for index page

--- valleybookreviews/home/views.py
+++ valleybookreviews/home/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, Blueprint
+from valleybookreviews.reviews.classes import UserReviews
 
 home = Blueprint("home", __name__)
 
 
 @home.route('/')
 def index():
-    return render_template("index.html")
+
+    random_review = UserReviews.get_random_review()
+    return render_template("index.html", random_review=random_review)
